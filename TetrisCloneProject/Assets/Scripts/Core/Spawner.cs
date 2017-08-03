e4f3c56cb9ba7231e498ed2bcf4ede9a2ead046c
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2017-08-02 23:08:19 -0400

    Create basic spawn and game controller methods

--- TetrisCloneProject/Assets/Scripts/Core/Spawner.cs
+++ TetrisCloneProject/Assets/Scripts/Core/Spawner.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
+    public Shape[] allShapes;
+
+    Shape GetRandomShape() {
+        int i = Random.Range(0, this.allShapes.Length);
+
+        if (this.allShapes[i]) {
+            return this.allShapes[i];
+        } else {
+            Debug.Log("WARNING! Invalid shape!");
+            return null;
+        }
+    }
+
+    public Shape SpawnShape() {
+        Shape shape = null;
+        shape = Instantiate(GetRandomShape(), transform.position, Quaternion.identity) as Shape;
+        if (shape) {
+            return shape;
+        } else {
+			Debug.Log("WARNING! Invalid shape is in spawner!");
+			return null;
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 		
--- TetrisCloneProject/Assets/Scripts/Managers/GameController.cs
+++ TetrisCloneProject/Assets/Scripts/Managers/GameController.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
+    // Reference to our game board
     Board gameBoard;
+
+    // Reference to our spawner
     Spawner spawner;
 
 	// Use this for initialization
 	void Start () {
-		
+        gameBoard = GameObject.FindWithTag("Board").GetComponent<Board>();
+        spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        if (!this.gameBoard) {
+            Debug.LogWarning("WARNING! There is no game board defined!");
+        }
+
+        if (!this.spawner) {
+            Debug.LogWarning("WARNING! There is no spawner defined!");
+        }
 	}
 	
 	// Update is called once per frame
