64d0202fe692c5a5124b2cc7ea53c242bbb30c62
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2017-08-02 22:49:22 -0400

    Create Shape methods and spawner and game controller scripts

--- TetrisCloneProject/Assets/Scripts/Core/Shape.cs
+++ TetrisCloneProject/Assets/Scripts/Core/Shape.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour {
 
+    public bool canRotate = true;
+
+    void Move(Vector3 moveDirection) {
+        transform.position += moveDirection;
+    }
+
+    public void MoveLeft() {
+        Move(new Vector3(-1, 0, 0));
+    }
+
+    public void MoveRight() {
+        Move(new Vector3(1, 0, 0)); 
+    }
+
+    public void MoveDown() {
+        Move(new Vector3(0, -1, 0));
+    }
+
+    public void MoveUp() {
+        Move(new Vector3(0, 1, 0));
+    }
+
+    public void RotateRight() {
+        if (this.canRotate) {
+            transform.Rotate(0, 0, -90);
+        }
+    }
+
+	public void RotateLeft() {
+		if (this.canRotate) {
+			transform.Rotate(0, 0, 90);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
 		
