126d4df7ffc34b55a64601d3fe1a47bd822991c0
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2017-09-03 21:49:16 -0400

    Created SoundManager and added sound clips to assets

---
+++ TetrisCloneProject/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public bool musicEnabled = true;
+    public bool fxEnabled = true;
+
+    [Range(0,1)]
+    public float musicVolume = 1.0f;
+
+    [Range(0,1)]
+    public float fxVolume = 1.0f;
+
+    public AudioClip clearRowSound;
+    public AudioClip moveSound;
+    public AudioClip dropSound;
+    public AudioClip gameOverSound;
+    public AudioClip backgroundMusic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
